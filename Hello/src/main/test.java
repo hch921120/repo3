524fdc2a0ca0c2001044cbe2872982a1e723113a
hch921120 <969610124@qq.com>
2019-06-10 16:43:58 +0800

    添加一句helloWorld2

--- Hello/src/main/test.java
+++ Hello/src/main/test.java
@@ -10,5 +10,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("hello worl2");
+        System.out.println("hello worl3");
     }
 }
