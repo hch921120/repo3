0b443131ab91b668a2c5dfcc3d829a2831700bde
hch921120 <969610124@qq.com>
2019-06-10 16:29:55 +0800

    idea 第一次提交项目

---
+++ Hello/src/main/test.java
@@ -0,0 +1,13 @@
+package main;
+
+/**
+ * @Auther: Hch
+ * @Date: 2019/6/10 16:27
+ * @Description:
+ */
+public class test {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
